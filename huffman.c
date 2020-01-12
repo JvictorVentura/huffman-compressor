bab41d0dd3bcbb8667885afd72d4bdce10e70fc7
J. Victor R. Ventura <joaovventura@protonmail.com>
2020-01-12 16:52:18 -0400

    added funtion that adds node to the head of a linked list

--- huffman.c
+++ huffman.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 typedef char byte;
 
 typedef struct Node{
@@ -9,9 +10,47 @@ typedef struct Node{
 	struct Node *right;
 	struct Node *left;
 
+	struct Node *next;
+
 } Node;
 
-int main(){
+
+void addNodeLinkedList(Node **head, char character){
+
+	Node *node = malloc(sizeof(Node));
 	
+	if(*head != NULL){
+		node->next = *head;
+		*head = node;
+	}else{
+		*head = node;
+	}
+
+	node->type = 1;
+	node->character = character;
+	node->quantity = 1;
+
+}
+
+int printLinkedList(Node *head){
+	if(head == NULL) return 1;
+
+
+	while(head->next != NULL){
+		printf("[%c, %i] ", head->character, head->quantity);
+		head = head->next;
+	}
+	
+	return 0;
+}
+
+
+
+int main(){
+	Node *head;
+	addNodeLinkedList(&head, 'a');
+	printLinkedList(head);
+	free(head);	
+
 	return 0;
 }
